857470f0b67f80550dab3cff4eb47b10853aa009
VhWeb <centusv@mail.ru>
2020-02-26 14:23:44 +0200

    dlc1

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-let money = +prompt("Ваш бюджет на месяц?", ""),
+//чтобы вопросы не выводились просто так пишем для этого функцию
+
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", "");
     time = prompt("Введите дату в формате YYYY-MM-DD", "YYYY-MM-DD");
+    //проверка данных на число, пустую строку, или ничего введенных в поле
+    while (isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", "");
+    }
+}
+start();
 
 let appData = {
     budget: money,
@@ -10,18 +21,21 @@ let appData = {
     savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-        b = prompt("Во сколько обойдется?", "");
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log("done");
-        appData.expenses[a] = b;
-    } else {
-        // повторяет цикл если неправильно введены данные
-        i = i - 1; 
+function chooseExpences() {
+    for (let i = 0; i < 2; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+            b = prompt("Во сколько обойдется?", "");
+        //проверка данных на строку и на то что в полях ничего не будет, и на то что в полях не будет пустых строк, и на количество символов введенных данных
+        if ((typeof (a)) === 'string' && (typeof (a)) != null && (typeof (b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log("done");
+            appData.expenses[a] = b;
+        } else {
+            // повторяет цикл если неправильно введены данные
+            i = i - 1;
+        }
     }
 }
-
+chooseExpences();
 // let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
 //     b = prompt("Во сколько обойдется?", "");
 // do {
@@ -29,16 +43,49 @@ for (let i = 0; i < 2; i++) {
 //     appData.expenses[a] = b;
 // } while ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50);
 
-appData.moneyPerDay = Math.round(appData.budget/30);
-
-alert("Ежедневный бюджет: " + appData.moneyPerDay);
-
-if (appData.moneyPerDay < 100) {
-    console.log("Малый уровень достатка");
-} else if ( appData.moneyPerDay > 100 && appData.moneyPerDay < 2000 ) {
-    console.log("Средний уровень достатка");
-} else if ( appData.moneyPerDay > 2000 ) {
-    console.log("Высокий уровень достатка");
-} else {
-    console.log("Произошла ошибка");
+function detectDayBudget() {
+    appData.moneyPerDay = Math.round(appData.budget / 30);
+    alert("Ежедневный бюджет: " + appData.moneyPerDay);
 }
+
+detectDayBudget();
+
+function detectLevel() {
+    if (appData.moneyPerDay < 100) {
+        console.log("Малый уровень достатка");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log("Средний уровень достатка");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log("Высокий уровень достатка");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+detectLevel();
+
+//функция высчитывает накопления с депозита если он есть
+function checkSavings() {
+    if (appData.savings == true) {
+        let save = +prompt("Какова сумма накоплений?"),
+            percent = +prompt("Под какой процент?");
+
+        appData.monthIncome = save / 100 / 12 * percent;
+        alert("Доход в месяц с вашего депозита: " + appData.monthIncome);
+    }
+}
+
+checkSavings();
+
+function chooseOptExpenses() {
+    for (let i = 1; i < 4; i++) {
+        let qr = prompt("Статья необязательных расходов?");
+        if ((typeof (qr)) === 'string' && (typeof (qr)) != null && qr != '' && qr.length < 50) {
+            appData.optionalExpenses[i] = qr;
+        } else {
+            // повторяет цикл если неправильно введены данные
+            i = i - 1;
+        }
+    }
+}
+chooseOptExpenses()
+console.log(appData);
