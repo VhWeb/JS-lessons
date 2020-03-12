69f5db772e5e5bede806fbfa5cfef739aa9fec89
VhWeb <centusv@mail.ru>
2020-03-12 21:51:05 +0200

    ajax

--- lesson 2/js/script.js
+++ lesson 2/js/script.js
@@ -107,4 +107,55 @@ window.addEventListener('DOMContentLoaded', function(){
         more.classList.remove('.more-splash');
         document.body.style.overflow = '';
     });
+        //Отправка формы
+    //Создаем объект с сообщениями состояния нашего запроса
+    let message = {
+        loading: "Загрузка...",
+        success: "Спасибо! Мы скоро с вами свяжемся!",
+        failure: "Что-то пошло не так..."
+    };
+    //Получаем форму и ее элементы, и создаем новый блок с ответом пользователю
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+    
+    form.addEventListener('submit', function(event) {
+        //отменяем перезагрузку страницы
+        event.preventDefault();
+        //добавляем в форм блок
+        form.appendChild(statusMessage);
+        //скрипт отправки данных на сервер создавая новый объект
+        let request = new XMLHttpRequest();
+        //настраиваем наш запрос
+        request.open('POST', 'server.php');//POST - т.к. мы отпраавляем наши данные на наш файл сервера
+        request.setRequestHeader('Content-Type', 'application/json', 'charset=utf-8');
+        //получаем данные которые ввел пользователь используя встроенный объект в котором лежат данные которые ввел пользователь
+        let formData = new FormData(form);
+        //создаем промежуточный пустой объект
+        let obj = {};
+        //превращаем объект formData в обычный используя obj
+        formData.forEach(function(value, key){
+            obj[key] = value;
+        });
+        //превращаем наш объект obj в json
+        let json = JSON.stringify(obj);
+        //отправляем наш запрос на сервер
+        request.send(json);
+        //пишем событие добавляющее сообщение на страницу используя отслеживание изменения состояния страницы(readyState)
+        request.addEventListener('readystatechange', function(){
+            if(request.readyState === 4) {
+                statusMessage.innerHTML = message.loading;
+            } else if (request.readyState === 4 || request.status == 200) {
+                statusMessage.innerHTML = message.success;
+            } else {
+                statusMessage.innerHTML = message.failure;
+            }
+        });
+        //очищаем наш инпут после отправки запроса
+        for(let i = 0; i < input.length; i++) {
+            input[i].value = '';
+        }
+    });
 });
